03fea24aa2797e8615a44e0594705bb6d5e4aa0e
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2023-06-18 10:30:37 +0200

    update

--- back/src/main/java/com/openclassrooms/bobapp/service/JokeService.java
+++ back/src/main/java/com/openclassrooms/bobapp/service/JokeService.java
@@ -1,33 +1,27 @@
 package com.openclassrooms.bobapp.service;
 
-import java.util.List;
-import java.util.Random;
-
-import org.springframework.stereotype.Service;
-
 import com.openclassrooms.bobapp.data.JsonReader;
 import com.openclassrooms.bobapp.model.Joke;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class JokeService {
-    
+
     private final JsonReader jsonReader;
 
     JokeService(JsonReader jsonReader) {
         this.jsonReader = jsonReader;
     }
 
-//    public Joke getRandomJoke() {
-//        List<Joke> jokes = this.jsonReader.getJokes();
-//        Random generator = new Random();
-//        int randomIndex = generator.nextInt(jokes.size());
-//        return jokes.get(randomIndex);
-//    }
 
     public Joke getRandomJoke() {
         List<Joke> jokes = this.jsonReader.getJokes();
-        int randomIndex = (int) (Math.random() * jokes.size());
+        int randomIndex = ThreadLocalRandom.current().nextInt(jokes.size());
         return jokes.get(randomIndex);
     }
+
 
 }
